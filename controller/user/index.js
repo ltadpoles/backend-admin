1df8eb72ba2f072bfca4a8db2948001a7ea43bed
tadpole <ltadpole@163.com>
2023-08-30 00:02:39 +0800

    user

--- controller/user/index.js
+++ controller/user/index.js
@@ -16,21 +16,28 @@ class User {
   async update(req, res) {
     const auth = req.auth
     const user = UserSchema.findOne({ where: { username: req.body.username } })
-    // 超级管理员
-    if (auth.state === 0) {
-      const { avatar, name, sex, phone, email, address, dec } = req.body
-
+    const { avatar, name, sex, phone, email, address, dec } = req.body
+    const updateData = async () => {
       try {
-        await UserSchema.update({ dec }, {
+        await UserSchema.update({ avatar, name, sex, phone, email, address, dec }, {
           where: {
             username: req.body.username
           }
         });
-
         res.send(result.success({ msg: '更新成功' }))
       } catch (err) {
         return res.send(result.fail({ msg: err.message }))
       }
+    }
+    // 超级管理员
+    if (auth.state === 0) {
+      updateData()
+    } else {
+      // 如果登录用户与要修改的用户不同
+      if (user.username !== auth.username) {
+        return res.send(result.fail({ code: '403', msg: '权限不足，请联系管理员' }))
+      }
+      updateData()
     }
   }
 
