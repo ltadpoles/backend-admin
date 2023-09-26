34881ce5389d4bd1efc1b650b7b8600c68bc56e4
tadpole <ltadpole@163.com>
2023-09-26 23:29:13 +0800

    article

--- routes/article.js
+++ routes/article.js
@@ -8,6 +8,6 @@ router.post('/list', Article.list)
 router.get('/del', Article.del)
 router.post('/update', Article.update)
 router.get('/info', Article.info)
-router.get('top-change', Article.topChange)
+router.get('/top-change', Article.topChange)
 
 export default router
